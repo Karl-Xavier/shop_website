8be7814da8805acbcdbb928e1e435496c1afe267
Karl-Xavier <uwasbruno256@gmail.com>
2024-05-19 14:38:23 +0100

    Modified the styling for the UI

--- src/pages/shop/Products.js
+++ src/pages/shop/Products.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { ShoppingCart } from "phosphor-react";
 import { ShopContext } from "../../context/ShopContext";
+import './Products.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 /* import { Col } from "react-bootstrap"; */
@@ -9,13 +10,13 @@ function Products(props){
 
     const {addToCart} = useContext(ShopContext)
     return (
-        <div class="col-lg-4 col-md-6 my-3">
-        <div class="displayCards card" style={{width: "18rem"}}>
+        <div class="col-lg-4 col-md-6 col-6 my-3">
+        <div class="displayCards card" style={{width: "100%"}}>
             <img class="rounded card-img-top" src={productImg} alt="" height={350}/>
           <div class="card-body">
             <h2>{productName}</h2>
             <p className="priceTag">$ {price}.00 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span style={{backgroundColor: "grey", fontWeight: "bold", padding: "4px", textDecoration: "line-through"}}>99%</span></p>
-            <button class="btn btn-lg btn-block btn-outline-dark" type="button" onClick={() => {addToCart(id)}}>Add to Cart <ShoppingCart/></button>
+            <button class="btn btn-lg btn-block btn-outline-dark cartbtn" type="button" onClick={() => {addToCart(id)}}>Add to Cart <ShoppingCart/></button>
           </div>
         </div>
       </div>
